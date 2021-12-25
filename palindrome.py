170a543b5cc1c44c171e784039c28504a17a56aa
Adam Aaronson <adamaaronson@gmail.com>
2021-12-25 16:03:31 -0600

    Change generator to print k different palindromes

--- palindrome.py
+++ palindrome.py
@@ -2,10 +2,10 @@ from collections import defaultdict
 import sys
 
 def get_prefixes(word):
-    return [word[:i] for i in range(len(word), 0, -1)]
+    return [word[:i] for i in range(len(word), 1, -1)]
 
 def get_suffixes(word):
-    return [word[i:] for i in range(len(word))]
+    return [word[i:] for i in range(1, len(word))]
 
 def get_prefix_list(wordlist):
     """Returns prefix dictionary of wordlist"""
@@ -31,20 +31,37 @@ def get_suffix_list(wordlist):
     return suffixes
 
 
-def make_palindrome(first, wordlist, maxwords=30, minlen=0):
-    """Returns a palindrome starting with the given first word using the words in wordlist"""
-    wordlist = [w for w in wordlist if len(w) >= minlen]
+class PalindromeCounter:
+    def __init__(self, max_palindromes):
+        self.palindromes_left = max_palindromes
+        self.palindromes_found = set()
+    
+    def print_palindrome(self, beginning, end):
+        palindrome = ' '.join(beginning + end)
+        if palindrome in self.palindromes_found:
+            return
+        
+        print(palindrome)
+
+        self.palindromes_left -= 1
+        self.palindromes_found.add(palindrome)
+
+
+def print_palindromes(first, wordlist, max_palindromes=50, max_words=30, min_len=0):
+    wordlist = [w for w in wordlist if len(w) >= min_len]
 
     prefixes = get_prefix_list(wordlist)
     suffixes = get_suffix_list(wordlist)
     wordset = set(wordlist)
 
-    return make_palindrome_with([first], [], first, '', wordset, prefixes, suffixes, maxwords)
+    counter = PalindromeCounter(max_palindromes)
+
+    print_palindromes_with([first], [], first, '', wordset, prefixes, suffixes, counter, max_words)
 
 
-def make_palindrome_with(beginning, end, front, back, wordset, prefixes, suffixes, maxwords):
+def print_palindromes_with(beginning, end, front, back, wordset, prefixes, suffixes, counter, max_words):
     """
-    Recursive helper method for make_palindrome
+    Recursive helper method for print_palindromes
 
     beginning: words already set at the beginning of the palindrome
     end: words already set at the end of the palindrome
@@ -52,23 +69,22 @@ def make_palindrome_with(beginning, end, front, back, wordset, prefixes, suffixe
     back: back of the frontier; letters before the end that aren't part of a word yet
     """
 
-    # print(beginning, end, front, back)
+    if counter.palindromes_left <= 0:
+        return
 
-    if len(beginning) + len(end) > maxwords:
-        return None
+    if len(beginning) + len(end) > max_words:
+        return
 
     if not front and not back:
-        return beginning + end # everything is matched
-    
-    if front and back:
-        return None # this shouldn't happen I think
+        counter.print_palindrome(beginning, end) # everything is matched
+        return
     
     if front:
         front_rev = front[::-1]
 
         # if the front is a palindrome, we good
         if front == front_rev: 
-            return beginning + end
+            counter.print_palindrome(beginning, end)
         
         # find reverses of beginnings of front, if there are any
         for i in range(len(front), 0, -1):
@@ -78,19 +94,13 @@ def make_palindrome_with(beginning, end, front, back, wordset, prefixes, suffixe
             new_word = front_prefix[::-1]
 
             if new_word in wordset:
-                result = make_palindrome_with(beginning, [new_word] + end, new_front, '', wordset, prefixes, suffixes, maxwords)
-
-                if result:
-                    return result
+                print_palindromes_with(beginning, [new_word] + end, new_front, '', wordset, prefixes, suffixes, counter, max_words)
         
         # find words that end in the front reversed, if there are any
         if front_rev in suffixes:
             for new_back in suffixes[front_rev]:
                 new_word = new_back + front_rev
-                result = make_palindrome_with(beginning, [new_word] + end, '', new_back, wordset, prefixes, suffixes, maxwords)
-
-                if result:
-                    return result
+                print_palindromes_with(beginning, [new_word] + end, '', new_back, wordset, prefixes, suffixes, counter, max_words)
     
 
     if back:
@@ -98,7 +108,7 @@ def make_palindrome_with(beginning, end, front, back, wordset, prefixes, suffixe
 
         # if the back is a palindrome, we good
         if back == back_rev: 
-            return beginning + end
+            counter.print_palindrome(beginning, end)
         
         # find reverses of beginnings of back, if there are any
         for i in range(len(back)):
@@ -108,21 +118,13 @@ def make_palindrome_with(beginning, end, front, back, wordset, prefixes, suffixe
             new_word = back_suffix[::-1]
 
             if new_word in wordset:
-                result = make_palindrome_with(beginning + [new_word], end, '', new_back, wordset, prefixes, suffixes, maxwords)
-
-                if result:
-                    return result
+                print_palindromes_with(beginning + [new_word], end, '', new_back, wordset, prefixes, suffixes, counter, max_words)
         
         # find words that start with the back reversed, if there are any
         if back_rev in prefixes:
             for new_front in prefixes[back_rev]:
                 new_word = back_rev + new_front
-                result = make_palindrome_with(beginning + [new_word], end, new_front, '', wordset, prefixes, suffixes, maxwords)
-
-                if result:
-                    return result
-    
-    return None
+                print_palindromes_with(beginning + [new_word], end, new_front, '', wordset, prefixes, suffixes, counter, max_words)
 
 
 def main(args):
@@ -130,12 +132,7 @@ def main(args):
         wordlist = f.read().splitlines()
         wordlist = [w.split(';')[0].upper() for w in wordlist]
     
-    palindrome = make_palindrome(args[1], wordlist, minlen=3)
-
-    if palindrome:
-        print(' '.join(palindrome))
-    else:
-        print('No palindrome found. :(')
+    print_palindromes(args[1], wordlist, max_palindromes=50, max_words=10)
 
 
 if __name__ == '__main__':
